45db5460d31b4ff475b9efb5b081633c45abe99e
lijunqian <lijunqian@dhms.net>
2019-06-12 19:19:31 +0800

    add README

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ UIPREFIX = TumConf
 SRC = mainUI.py
 all:
 	pyuic5 $(UIPREFIX).ui -o $(UIPREFIX).py
-
+	python $(SRC)
 clean:
 	rm -rf __pycache__
---
+++ README.mk
@@ -0,0 +1,25 @@
+# 一个产生网络配置(json)的工具
+## 开发环境
+* MacOS Mojave
+* VSCode Version 1.35.0 (1.35.0)
+* Python Python 3.6.4
+* PyQT5
+* QT Designer 5.12.3
+
+## 使用说明
+### MacOS/Linux
+> make 
+
+或者
+> python mainUI.py
+### Window
+python mainUI.py
+
+
+
+## 开发遇到整理
+### vscode unable to import 'pyqt5'
+https://stackoverflow.com/a/51010506/2422318
+
+### AttributeError: module 'PyQt5.QtGui' has no attribute 'QMainWindow'
+https://blog.csdn.net/weixin_42137589/article/details/81489363
